0a7d87b2bd0a0505237327e7b8aa63030e9e5a48
Ramiro Gonzalez <ramirogc21@gmail.com>
2024-02-06 14:55:27 -0300

    update contract ref

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShowAsset from './ShowAsset';
 
 import DecentralizedDataMarketJson from './utils/DecentralizedDataMarket.json';
 const contractABI = DecentralizedDataMarketJson.abi;
-const CONTRACT_ADDRESS = '0xD5a640fAC82FD990D8594b4618baE3ED56ADE066';
+const CONTRACT_ADDRESS = '0x88a28fad7be69C626C288B931b1ad852eff8942b';
 
 function App() {
 
